71e605b049099028bf5cd1085b9d278edfcf9fe4
Fayyez <fayyez2056@gmail.com>
2023-12-10 20:21:29 +0500

    add hoomepage

--- src/home_page_ui.py
+++ src/home_page_ui.py
@@ -20,7 +20,8 @@ folder_path = os.getcwd() + "/src"
 class Ui_homepage(object):
     currentwindow =None
     
-    def setupUi(self, mainwindow, classrooms: list):
+    def setupUi(self, mainwindow, classrooms: dict):
+        print(classrooms)
         currentwindow = mainwindow
         mainwindow.setObjectName("homepage")
         mainwindow.resize(1000, 700)
@@ -43,6 +44,13 @@ class Ui_homepage(object):
         self.search_line_edit.setStyleSheet("border-radius:10px;\n"
 "background-color:#fbeaeb")
         self.search_line_edit.setObjectName("search_line_edit")
+        # search completer
+        # get a list of class titles from class room dict values
+        classTitiles = [classrooms[classs].title for classs in classrooms]
+        completer = QtWidgets.QCompleter(classTitiles)
+        completer.setCaseSensitivity(QtCore.Qt.CaseInsensitive)
+        self.search_line_edit.setCompleter(completer)
+
         self.top_widget_2 = QtWidgets.QWidget(mainwindow)
         self.top_widget_2.setGeometry(QtCore.QRect(0, 0, 981, 61))
         self.top_widget_2.setStyleSheet("background-color:#fbeaeb")
@@ -50,16 +58,18 @@ class Ui_homepage(object):
         self.back_button = QtWidgets.QPushButton(self.top_widget_2)
         self.back_button.setGeometry(QtCore.QRect(10, 10, 61, 41))
         self.back_button.setIconSize(QtCore.QSize(30, 30))
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(folder_path+"/assets/back.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.back_button.setIcon(icon)
-"")
+        back_icon = QtGui.QIcon()
+        back_icon.addPixmap(QtGui.QPixmap(folder_path+"/assets/previous.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.back_button.setIcon(back_icon)
         self.back_button.setText("")
         self.back_button.setObjectName("back_button")
         self.hamburegr_button = QtWidgets.QPushButton(self.top_widget_2)
         self.hamburegr_button.setGeometry(QtCore.QRect(920, 10, 51, 41))
-        self.hamburegr_button.setStyleSheet("border-image: url(/Users/Zulfiqar/Pictures/menu.PNG) stretch;\n"
-"")
+        hamburger_icon = QtGui.QIcon()
+        hamburger_icon.addPixmap(QtGui.QPixmap(folder_path+"/assets/menu.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        # increase the size of the icon
+        self.hamburegr_button.setIconSize(QtCore.QSize(40, 40))
+        self.hamburegr_button.setIcon(hamburger_icon)
         self.hamburegr_button.setText("")
         self.hamburegr_button.setObjectName("hamburegr_button")
         self.homepageLabel = QtWidgets.QLabel(self.top_widget_2)
@@ -77,16 +87,55 @@ class Ui_homepage(object):
         self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.gridLayout.setObjectName("gridLayout")
+        ## adding the classrooms to the grid layout
+        #create two cards of fisrt 2 value of the classroom dict
+        i= 0
+        for classs in classrooms:
+                # create a card widget
+                card = QtWidgets.QWidget()
+                card.setStyleSheet("background-color: #fbeaeb; border-radius: 10px;")
+                # create a vertical layout for the card
+                verticalLayout = QtWidgets.QVBoxLayout(card)
+                verticalLayout.setObjectName("verticalLayout")
+                # create a label for the class title
+                class_title_label = QtWidgets.QLabel(card)
+                class_title_label.setText(classrooms[classs].title)
+                class_title_label.setStyleSheet("font-size: 20px; font-weight: bold; color: #2f3c7e;")
+                class_title_label.setAlignment(QtCore.Qt.AlignCenter)
+                verticalLayout.addWidget(class_title_label)
+                # create a label for the number of students
+                number_of_students_label = QtWidgets.QLabel(card)
+                number_of_students_label.setText("class id: "+str(classrooms[classs].class_id))
+                number_of_students_label.setStyleSheet("font-size: 15px; color: #2f3c7e;")
+                number_of_students_label.setAlignment(QtCore.Qt.AlignCenter)
+                verticalLayout.addWidget(number_of_students_label)
+                # create a label for the number of students
+                number_of_teachers_label = QtWidgets.QLabel(card)
+                number_of_teachers_label.setText("Number of students: "+str(classrooms[classs].studentount))
+                number_of_teachers_label.setStyleSheet("font-size: 15px; color: #2f3c7e;")
+                number_of_teachers_label.setAlignment(QtCore.Qt.AlignCenter)
+                verticalLayout.addWidget(number_of_teachers_label)
+                # create a label for the number of students
+                # add the card to the grid layout
+                self.gridLayout.addWidget(card, i, 0, 1, 1)
+                i+=1
+
         self.filter_button = QtWidgets.QPushButton(mainwindow)
-        self.filter_button.setGeometry(QtCore.QRect(550, 90, 51, 41))
-        self.filter_button.setStyleSheet("border-image: url(/Users/Zulfiqar/Pictures/clear.PNG) stretch;\n"
-"")
+        self.filter_button.setGeometry(QtCore.QRect(545, 88, 51, 45))
+        filter_icon = QtGui.QIcon()
+        filter_icon.addPixmap(QtGui.QPixmap(folder_path+"/assets/filter.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        # increase the size of the icon
+        self.filter_button.setIconSize(QtCore.QSize(40, 40))
+        self.filter_button.setIcon(filter_icon)
         self.filter_button.setText("")
         self.filter_button.setObjectName("filter_button")
         self.add_room_pushButton = QtWidgets.QPushButton(mainwindow)
-        self.add_room_pushButton.setGeometry(QtCore.QRect(780, 320, 91, 81))
-        self.add_room_pushButton.setStyleSheet("border-image: url(/Users/Zulfiqar/Pictures/ad.PNG) stretch;\n"
-"")
+        self.add_room_pushButton.setGeometry(QtCore.QRect(780, 350, 91, 81))
+        add_roon_icon = QtGui.QIcon()
+        add_roon_icon.addPixmap(QtGui.QPixmap(folder_path+"/assets/u2.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        # increase the size of the icon
+        self.add_room_pushButton.setIconSize(QtCore.QSize(60, 60))
+        self.add_room_pushButton.setIcon(add_roon_icon)
         self.add_room_pushButton.setText("")
         self.add_room_pushButton.setObjectName("add_room_pushButton")
         self.homepageLabel_2 = QtWidgets.QLabel(mainwindow)
@@ -109,6 +158,10 @@ class Ui_homepage(object):
         self.search_line_edit.setPlaceholderText(_translate("homepage", " Search......"))
         self.homepageLabel.setText(_translate("homepage", "Home Page"))
         self.homepageLabel_2.setText(_translate("homepage", "+ Add Room"))
+        # create a boundary for homepageLabel_2
+        self.homepageLabel_2.setStyleSheet("border: 2px solid black; border-radius: 10px; background-color: #fbeaeb;")
+
+    #def 
 
 
 if __name__ == "__main__":
@@ -116,6 +169,6 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     homepage = QtWidgets.QWidget()
     ui = Ui_homepage()
-    ui.setupUi(homepage)
+    ui.setupUi(homepage, {'1': ClassRoom('class 1', 8, 'sn_123', '1'), '2': ClassRoom('class 2', 10, 'sn_123', '2'), '5': ClassRoom('class 1', 8, 'sn_123', '1'), '6': ClassRoom('class 1', 8, 'sn_123', '1'),} )
     homepage.show()
     sys.exit(app.exec_())
