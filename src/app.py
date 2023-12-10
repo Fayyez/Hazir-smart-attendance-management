c2fd13c5f1d9d29e0a63ca7c6432c5645ee9c606
Fayyez <fayyez2056@gmail.com>
2023-12-10 05:53:38 +0500

    clear structuring

--- src/app.py
+++ src/app.py
@@ -1,13 +1,13 @@
-# # this file basically runs the entire system, renders the relevant screen and calls the relevant functions
-# # from utilis
-# # import everything here
-from frontend.loginscreen import Ui_Dialog
-if __name__ == "__main__":
- screen_stack= []
- Login_screen=Ui_Dialog()
- screen_stack.append(Login_screen)
- teacherid = screen_stack[-1].show_screen()
- print(teacherid)
+# # # this file basically runs the entire system, renders the relevant screen and calls the relevant functions
+# # # from utilis
+# # # import everything here
+# from frontend.loginscreen import Ui_Dialog
+# if __name__ == "__main__":
+#  screen_stack= []
+#  Login_screen=Ui_Dialog()
+#  screen_stack.append(Login_screen)
+#  teacherid = screen_stack[-1].show_screen()
+#  print(teacherid)
 
 
 
